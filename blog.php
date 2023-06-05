172c8afe192b6637ccf5397ab927a3a691eed154
Dragon2517 <rabvasya1994@yandex.ru>
2023-06-05 21:19:59 +0300

    0

---
+++ blog.php
@@ -0,0 +1,23 @@
+<?php
+// Файл для создание/пересоздания БД
+
+
+if (file_exists('blog.sqlite')) {
+    unlink('blog.sqlite');
+}
+
+
+$pdo = new PDO('sqlite:blog.sqlite');
+
+
+
+$pdo->exec(
+    'CREATE TABLE users (
+    Id INT PRIMARY KEY,
+    Username TEXT,
+    Phone INT  NOT NULL,
+    Email  TEXT NOT NULL,
+    Date_of_birth TEXT,
+    Photo TEXT
+    )'
+);
